6a9668b332bc7ae3c43b516df58bcbeddf42c050
edu-arvlin <arvid.lind@skola.taby.se>
2026-04-02 12:36:10 +0200

    Gjort klart projektet
    Fungerande program som gör klart miniprojekt 1

--- CitatBanken.py
+++ CitatBanken.py
@@ -22,3 +22,62 @@ def spara_citat(citat_lista):
             fil.write(citat + "\n")
 
 
+# visa alla citat
+def visa_citat(citat_lista):
+    if len(citat_lista) == 0:
+        print("Inga citat sparade.\n")
+    else:
+        for i, citat in enumerate(citat_lista, 1):
+            print(f"{i}. {citat}")
+        print()
+
+
+# lägg till citat
+def lagg_till(citat_lista):
+    text = input("Skriv citat: ")
+    skrivare = input("Vem sa det?: ")
+    nytt = f'"{text}" - {skrivare}'
+    citat_lista.append(nytt)
+    spara_citat(citat_lista)
+    print("Citat sparat!\n")
+
+
+# visa slumpmässigt citat
+def slump_citat(citat_lista):
+    if len(citat_lista) == 0:
+        print("Inga citat att visa.\n")
+    else:
+        print(random.choice(citat_lista))
+        print()
+
+
+def main():
+    citat_lista = ladda_citat()
+
+    while True:
+        print("1. Visa alla citat")
+        print("2. Lägg till nytt citat")
+        print("3. Visa slumpmässigt citat")
+        print("4. Avsluta")
+
+        val = input("Välj: ")
+
+        if val == "1":
+            visa_citat(citat_lista)
+
+        elif val == "2":
+            lagg_till(citat_lista)
+
+        elif val == "3":
+            slump_citat(citat_lista)
+
+        elif val == "4":
+            print("Hejdå!")
+            break
+
+        else:
+            print("Fel val, försök igen.\n")
+
+
+# Starta programmet
+main()
